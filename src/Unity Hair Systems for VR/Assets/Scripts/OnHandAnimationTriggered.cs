dab5327186019562587987514bea554ac195aebe
KhangDuySaritasa <126656485+KhangDuySaritasa@users.noreply.github.com>
2023-11-09 18:54:55 +0700

    add hair,model,chair package

---
+++ src/Unity Hair Systems for VR/Assets/Scripts/OnHandAnimationTriggered.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.DemoTeam.Hair;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OnHandAnimationTriggered : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionProperty gripAction;
+
+    [SerializeField]
+    private Animator handAnimator;
+
+    private float gribInputValue;
+
+    private void Update()
+    {
+        HandGrib();
+    }
+
+    private void HandGrib()
+    {
+        gribInputValue = gripAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gribInputValue);
+    }
+}
